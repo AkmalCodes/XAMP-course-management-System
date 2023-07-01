045a005ce2bcd0c133a341d9122f774b33530bb9
badlmabda <akmalhisyam97@gmail.com>
2023-07-01 17:55:13 +0800

    added page for displaying forms to update availability for a course (only instructors) -razlan

---
+++ courseschedule_instructor.php
@@ -0,0 +1,26 @@
+<?php
+include 'utils/connect.php';
+include 'utils/session_start.php';
+include 'utils/sessionnull.php';
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Course schedule</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <div id="profile-container">
+        <?php include 'pageContent/nav.php'; ?>
+        <div class="grid-container">
+            <div class="table container">
+            <?php include 'pageContent/courseschedule_table.php'; ?>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
